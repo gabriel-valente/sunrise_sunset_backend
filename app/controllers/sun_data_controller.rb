f71e9be0b9b1d7e080a89eaa99cdeeef20249145
Gabriel Valente <gabrielmcv1@gmail.com>
2025-02-14 19:14:10 +0000

    Fix Start/end date

--- app/controllers/sun_data_controller.rb
+++ app/controllers/sun_data_controller.rb
@@ -2,19 +2,18 @@ class SunDataController < ApplicationController
   def index
     location = params[:location]
     start_date = params[:start_date]
-    end_date = params[:end_date]
+    end_date = params[:end_date] || params[:start_date] # If we dont have an end date, just use the start date so we have an interval
 
-    return render json: { error: "MISSING_PARAMETERS" }, status: :bad_request if location.blank? || (start_date.blank? && end_date.blank?) # We need the location and at least one date
+    return render(json: { error: "MISSING_PARAMETERS" }, status: :bad_request) if location.blank? || start_date.blank? # We need the location and at least one date
 
-    records = SunriseSunset.where(location: location, date: start_date..(end_date || start_date)).order(:date).to_a
+    records = SunriseSunset.where(location: location, date: start_date..end_date).order(:date).to_a
 
     # An array of the dates that are not present in the database
-    # Date.parse(end_date || start_date) = If we dont have an end date it used the start date
-    missing_dates = (Date.parse(start_date)..Date.parse(end_date || start_date)).to_a - records.pluck(:date)
+    missing_dates = (Date.parse(start_date)..Date.parse(end_date)).to_a - records.pluck(:date)
     missing_dates.map! { |date| date.strftime('%Y-%m-%d') }
 
     if missing_dates.empty? # If the array is empty, it means we have every date we needed
-      render json: records.as_json(except: [:id, :created_at, :updated_at])
+      render(json: records.as_json(except: [:id, :created_at, :updated_at]))
     else
       # Call the API for the range of dates we need
       # This is a pessimistic call, because we will get the dates in between the start/end even if we already have them
@@ -34,9 +33,9 @@ class SunDataController < ApplicationController
           )
         end
 
-        render json: records.sort_by(&:date).as_json(except: [:id, :created_at, :updated_at])
+        render(json: records.sort_by(&:date).as_json(except: [:id, :created_at, :updated_at]))
       else
-        render json: { error: data["message"] }, status: :unprocessable_entity
+        render(json: { error: data["message"] }, status: :unprocessable_entity)
       end
     end
   end
